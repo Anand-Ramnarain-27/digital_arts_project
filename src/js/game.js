5451fb01c0b304e286dfcc3826f30d6d03692a9c
Anand-Ramnarain-27 <anandram0207@gmail.com>
2024-09-04 16:59:35 +0200

    Game Over State

--- src/js/game.js
+++ src/js/game.js
@@ -1,119 +1,140 @@
 class Game {
 
-  constructor() {
-      G = this;
-      G.clock = 0;
+    constructor() {
+        G = this;
+        G.clock = 0;
 
-      this.level = LEVELS[0];
-      this.level.start();
+        this.altitude = 0;
 
-      this.altitude = 0;
-      this.centerLevel(this.level);
-  }
+        this.level = LEVELS[0];
+        this.startLevel(this.level);
+    }
 
-  cycle(e) {
-      this.clock += e;
+    cycle(e) {
+        this.clock += e;
 
-      this.level.cycle(e);
-      INTERPOLATIONS.slice().forEach(i => i.cycle(e));
+        this.level.cycle(e);
+        INTERPOLATIONS.slice().forEach(i => i.cycle(e));
 
-      this.render();
-  }
+        if (this.menu) {
+            this.menu.cycle(e);
+        }
 
-  centerLevel(level) {
-      const levelHeight = LEVEL_ROWS * CELL_SIZE;
-      const towerBaseHeight = (CANVAS_HEIGHT - levelHeight) / 2;
-      // this.altitude = this.levelY(level) - towerBaseHeight;
+        this.render();
+    }
 
-      interp(this, 'altitude', this.altitude, this.levelY(level) - towerBaseHeight, 0.5)
-  }
+    nextLevel() {
+        // Stop the previous level
+        this.level.stop();
 
-  levelY(level) {
-      const levelHeight = LEVEL_ROWS * CELL_SIZE;
-      const towerBaseHeight = (CANVAS_HEIGHT - levelHeight) / 2;
-      const levelBottomY = CANVAS_HEIGHT - level.index * levelHeight - towerBaseHeight;
+        // Start the new one
+        this.startLevel(LEVELS[this.level.index + 1]);
+    }
 
-      return levelBottomY - levelHeight;
-  }
+    startLevel(level) {
+        this.level = level;
+        this.centerLevel(this.level);
+        this.level.start();
+    }
 
-  // renderOneBuilding(x, altitude) {
-  //     wrap(() => {
-  //         translate(x, altitude);
-  //         R.fillStyle = WINDOW_PATTERN;
-  //
-  //         fs(0, 0);
-  //     });
-  // }
+    centerLevel(level) {
+        const levelHeight = LEVEL_ROWS * CELL_SIZE;
+        const towerBaseHeight = (CANVAS_HEIGHT - levelHeight) / 2;
+        // this.altitude = this.levelY(level) - towerBaseHeight;
 
-  render() {
-      const levelWidth = LEVEL_COLS * CELL_SIZE;
-      const levelHeight = LEVEL_ROWS * CELL_SIZE;
-      const levelX = (CANVAS_WIDTH - LEVEL_COLS * CELL_SIZE) / 2;
+        interp(this, 'altitude', this.altitude, this.levelY(level) - towerBaseHeight, 0.5)
+    }
 
-      // Sky
-      R.fillStyle = SKY_BACKGROUND;
-      fr(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // TODO maybe split into two?
+    levelY(level) {
+        const levelHeight = LEVEL_ROWS * CELL_SIZE;
+        const towerBaseHeight = (CANVAS_HEIGHT - levelHeight) / 2;
+        const levelBottomY = CANVAS_HEIGHT - level.index * levelHeight - towerBaseHeight;
 
-      R.fillStyle = '#fff';
-      beginPath();
-      arc(CANVAS_WIDTH - 200, 100, 50, 0, PI * 2, true);
-      fill();
+        return levelBottomY - levelHeight;
+    }
 
-      // Buildings
-      // const xRatio = this.level.player.x / (CELL_SIZE * LEVEL_COLS);
-      // const xRatioFromCenter = xRatio - 0.5;
-      // const perspective = xRatioFromCenter * 80;
-      //
-      // R.fillStyle = WINDOW_PATTERN;
-      // wrap(() => {
-      //     translate(levelX, 0);
-      //     scale(xRatioFromCenter, 1);
-      //     fr(0, 0, 120, CANVAS_HEIGHT);
-      // });
-      // wrap(() => {
-      //     translate(levelX + CELL_SIZE * LEVEL_COLS, 0);
-      //     scale(-xRatioFromCenter, 1);
-      //     fr(0, 0, -120, CANVAS_HEIGHT);
-      // });
+    // renderOneBuilding(x, altitude) {
+    //     wrap(() => {
+    //         translate(x, altitude);
+    //         R.fillStyle = WINDOW_PATTERN;
+    //
+    //         fs(0, 0);
+    //     });
+    // }
 
-      wrap(() => {
-          translate(0, -this.altitude);
+    render() {
+        const levelWidth = LEVEL_COLS * CELL_SIZE;
+        const levelHeight = LEVEL_ROWS * CELL_SIZE;
+        const levelX = (CANVAS_WIDTH - LEVEL_COLS * CELL_SIZE) / 2;
 
-          R.fillStyle = '#000';
-          fr(levelX, 0, levelWidth, CANVAS_HEIGHT);
+        // Sky
+        R.fillStyle = SKY_BACKGROUND;
+        fr(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // TODO maybe split into two?
 
-          const currentLevelIndex = LEVELS.indexOf(this.level);
-          for (let i = max(0, currentLevelIndex - 1) ; i < min(LEVELS.length, currentLevelIndex + 2) ; i++) {
-              wrap(() => {
-                  translate(levelX, this.levelY(LEVELS[i]));
-                  LEVELS[i].render();
-              });
-          }
-      });
+        R.fillStyle = '#fff';
+        beginPath();
+        arc(CANVAS_WIDTH - 200, 100, 50, 0, PI * 2, true);
+        fill();
+
+        // Buildings
+        // const xRatio = this.level.player.x / (CELL_SIZE * LEVEL_COLS);
+        // const xRatioFromCenter = xRatio - 0.5;
+        // const perspective = xRatioFromCenter * 80;
+        //
+        // R.fillStyle = WINDOW_PATTERN;
+        // wrap(() => {
+        //     translate(levelX, 0);
+        //     scale(xRatioFromCenter, 1);
+        //     fr(0, 0, 120, CANVAS_HEIGHT);
+        // });
+        // wrap(() => {
+        //     translate(levelX + CELL_SIZE * LEVEL_COLS, 0);
+        //     scale(-xRatioFromCenter, 1);
+        //     fr(0, 0, -120, CANVAS_HEIGHT);
+        // });
+
+        wrap(() => {
+            translate(0, -this.altitude);
+
+            R.fillStyle = '#000';
+            fr(levelX, 0, levelWidth, CANVAS_HEIGHT);
+
+            const currentLevelIndex = LEVELS.indexOf(this.level);
+            for (let i = max(0, currentLevelIndex - 1) ; i < min(LEVELS.length, currentLevelIndex + 2) ; i++) {
+                wrap(() => {
+                    translate(levelX, this.levelY(LEVELS[i]));
+                    LEVELS[i].render();
+                });
+            }
+        });
+
+        if (this.menu) {
+            wrap(() => this.menu.render());
+        }
 
 
-      if (DEBUG) {
-          wrap(() => {
-              R.font = '24pt Arial';
-              R.textAlign = 'left';
-              fs('#fff');
+        if (DEBUG) {
+            wrap(() => {
+                R.font = '24pt Arial';
+                R.textAlign = 'left';
+                fs('#fff');
 
-              const fpsGauge = [];
-              for (let i = 0 ; i < (G.fps / 60) * 20 ; i++) {
-                  fpsGauge.push('-');
-              }
+                const fpsGauge = [];
+                for (let i = 0 ; i < (G.fps / 60) * 20 ; i++) {
+                    fpsGauge.push('-');
+                }
 
-              const info = [
-                  'fps: ' + G.fps,
-                  fpsGauge.join(''),
-              ];
-              let y = 40;
-              info.forEach(info => {
-                  fillText(info, 40, y);
-                  y += 40;
-              });
-          });
-      }
-  }
+                const info = [
+                    'fps: ' + G.fps,
+                    fpsGauge.join(''),
+                ];
+                let y = 40;
+                info.forEach(info => {
+                    fillText(info, 40, y);
+                    y += 40;
+                });
+            });
+        }
+    }
 
 }
