490842e4f5b93784437db7db28cdb923c9c247db
Anand-Ramnarain-27 <anandram0207@gmail.com>
2024-09-04 11:58:23 +0200

    add to readd the player again
    Uses github it showed changes but never update it

--- src/js/game.js
+++ src/js/game.js
@@ -1,21 +1,26 @@
 class Game {
-  constructor() {
-    G = this;
-    G.clock = 0;
 
-    this.level = new Level(LEVELS[0]);
+  constructor() {
+      G = this;
+      G.clock = 0;
+
+      this.level = new Level(LEVELS[0]);
   }
 
-  cycle(e) {}
+  cycle(e) {
+      this.level.cycle(e);
+      this.render();
+  }
 
   render() {
-    R.fillStyle = "#585a7f";
-    fr(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      R.fillStyle = '#585a7f';
+      fr(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    wrap(() => {
-      translate((CANVAS_WIDTH - LEVEL_COLS * CELL_SIZE) / 2, 0);
+      wrap(() => {
+          translate((CANVAS_WIDTH - LEVEL_COLS * CELL_SIZE) / 2, 0);
 
-      this.level.render();
-    });
+          this.level.render();
+      });
   }
+
 }
--- src/js/main.js
+++ src/js/main.js
@@ -4,37 +4,35 @@ onload = () => {
   CANVAS.width = CANVAS_WIDTH;
   CANVAS.height = CANVAS_HEIGHT;
 
-  R = CANVAS.getContext("2d");
+  R = CANVAS.getContext('2d');
   // R.imageSmoothingEnabled = false;
 
   // Shortcut for all canvas methods to the main canvas
-  Object.getOwnPropertyNames(canvasProto).forEach((n) => {
-    if (R[n].call) {
-      w[n] = canvasProto[n].bind(R);
-    }
+  Object.getOwnPropertyNames(canvasProto).forEach(n => {
+      if (R[n].call) {
+          w[n] = canvasProto[n].bind(R);
+      }
   });
 
-  console.log("LOADED");
+  console.log('LOADED');
 
   new Game();
-  G.render();
-  return;
 
   // Start cycle()
   let lastFrame = Date.now();
   let frame = () => {
-    let n = Date.now(),
-      e = (n - lastFrame) / 1000;
+      let n = Date.now(),
+          e = (n - lastFrame) / 1000;
 
-    if (DEBUG) {
-      G.fps = ~~(1 / e);
-    }
+      if(DEBUG){
+          G.fps = ~~(1 / e);
+      }
 
-    lastFrame = n;
+      lastFrame = n;
 
-    G.cycle(e);
+      G.cycle(e);
 
-    requestAnimationFrame(frame);
+      requestAnimationFrame(frame);
   };
   frame();
 };
